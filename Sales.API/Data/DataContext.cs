152fc51cfb2b4abae136d50c97a8a7197ef2bda7
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-02-15 18:24:11 -0500

    Se crea la primer migracion del proyecto con la base de datos

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Este HasIndex es para que no se repitan los datos en paises, ejemplo : no pueden haber 2 paises llamados igual
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
