ac74d2f3fc9d57b400ee83406c97b6f00d6c894b
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-02-23 07:27:35 -0500

    Se crea el componente de CountryCreate y CountryForm

---
+++ Sales.WEB/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,31 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Sales.Shared.Entities;
+
+namespace Sales.WEB.Pages.Countries
+{
+    partial class CountryCreate
+    {
+        private Country country = new();
+        private CountryForm? countryForm;
+
+        private string url { get; set; } = "/api/countries";
+
+        private async Task Create()
+        {
+            var httpResponse = await repository.Post<Country, CountryForm>(url, country);
+            if (httpResponse.Error)
+            {
+                var message = await httpResponse.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+        }
+
+        private void Return()
+        {
+            countryForm!.FormPostedSuccesFully = true;
+            navigationManager.NavigateTo("/countries");
+        }
+    }
+}
