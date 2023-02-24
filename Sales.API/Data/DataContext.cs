50226def95ff3d06f7715bd7f842f8a65d927cdc
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-02-24 16:05:56 -0500

    Se crean las entidades de city,State

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -10,7 +10,9 @@ namespace Sales.API.Data
 
         }
 
+        public DbSet<City>? Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +20,10 @@ namespace Sales.API.Data
 
             //Este HasIndex es para que no se repitan los datos en paises, ejemplo : no pueden haber 2 paises llamados igual
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+
+            modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique(); // Esto es para los nombres no se repitan por pais, ejemplo en antioquia no va a ver mas de un rionegro
+
+            modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique(); //Esto es para que la ciudad no se repita por estado
         }
     }
 }
