94447b72de9486bc57fa9aabf2d3ee239e0bf9ee
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-03-08 16:51:13 -0500

    add pagination and filters

---
+++ Sales.API/Helpers/QueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Sales.Shared.DTOs;
+
+namespace Sales.API.Helpers
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO pagination)
+        {
+            return queryable
+                .Skip((pagination.Page - 1) * pagination.RecordsNumber) // es la cantidad de registros que usted se va a saltar
+                .Take(pagination.RecordsNumber);
+        }
+    }
+
+}
---
+++ Sales.Shared/DTOs/PaginationDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.DTOs
+{
+    public class PaginationDTO
+    {
+        public int Id { get; set; }
+        public int Page { get; set; } = 1; //Page es la pagina en la que estamos
+        public int RecordsNumber { get; set; } = 10;
+        public string? Filter { get; set; }
+    }
+}
