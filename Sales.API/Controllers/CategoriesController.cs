7546fcddaca4c35c30fe7933e41eb0afa5b84a85
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-03-10 13:59:12 -0500

    ultimos cambios del filtro en categorias

--- Sales.API/Controllers/CategoriesController.cs
+++ Sales.API/Controllers/CategoriesController.cs
@@ -25,6 +25,12 @@ namespace Sales.API.Controllers
         public async Task<IActionResult> GetAllCategory([FromQuery] PaginationDTO pagination)
         {
             var queryable = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
             return Ok(await queryable
                 .OrderBy(x => x.Name)
                 .Paginate(pagination)
@@ -36,10 +42,10 @@ namespace Sales.API.Controllers
         {
             var queryable = _context.Categories.AsQueryable();
 
-            //if (!string.IsNullOrWhiteSpace(pagination.Filter))
-            //{
-            //    queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
-            //}
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
 
             double count = await queryable.CountAsync();
             double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
