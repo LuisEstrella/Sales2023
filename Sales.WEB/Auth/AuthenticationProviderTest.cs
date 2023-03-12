7d5c1901de431f3a32ed67d611316b8768dfb7f4
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2023-03-12 11:03:43 -0500

    add Security and Autorization

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var EstrellaUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Luis"),
+            new Claim("LastName", "Estrella"),
+            new Claim(ClaimTypes.Name, "luisestre@yopmail.com"),
+            new Claim(ClaimTypes.Role, "User")
+
+        },
+        authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(EstrellaUser)));
+
+        }
+    }
+}
